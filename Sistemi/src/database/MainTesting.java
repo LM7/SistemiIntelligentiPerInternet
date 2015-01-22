c96ef3b7918222f32628f2572c0a301721d8dd90
dario <dario@10.25.252.11>
2015-01-22 15:48:52 +0100

    mongodb testing (date)

--- Sistemi/src/database/MainTesting.java
+++ Sistemi/src/database/MainTesting.java
@@ -1,5 +1,14 @@
 package database;
+
+import java.io.PrintWriter;
+import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.HashMap;
+
+import suTime.SUTime;
+import boilerpipe.Boilerpipe;
+
 import com.mongodb.*;
 
 public class MainTesting {
@@ -8,28 +17,61 @@ public class MainTesting {
 			MongoClient mongo = new MongoClient("localhost", 27017);
 			DB db = mongo.getDB("cose");
 			DBCollection collection = db.getCollection("libriCollection");
+			
+			BasicDBObject x = new BasicDBObject();
+			collection.remove(x);
+			
+			Boilerpipe b = new Boilerpipe();
+			
+			URL url = new URL("http://www.last.fm/event/3996308+Giraffage+at+Music+Hall+of+Williamsburg+on+31+January+2015");
+			
+			String title = b.getText(url)[0];
+			String text = b.getText(url)[1];
+		
+			
+			SUTime suT = new SUTime();			
+			HashMap<Date, Integer> date = suT.getTime(title,text);
+			
+			HashMap<String, Integer> gigio = new HashMap<String, Integer>();
+			
+			for (Date d : date.keySet()){
+				gigio.put(d.toString(), date.get(d));
+			}
 
+			
 			// inserisco un libro
 			BasicDBObject document = new BasicDBObject();
 			
-			document.put("isbn", 1935182870);
-			document.put("titolo", "MongoDB in Action");
-			document.put("autore", "Kyle Banker");
-			
-			document.put("gigio", "nomeGigio");
+			System.out.println(date.size());
+			System.out.println(gigio.size());
+			document.put("date", gigio);
+			document.put("data proposta", suT.dataEvento(date).toString());
 			collection.insert(document);
-
-			// ora lo vado a ricercare
+			
+			BasicDBObject document2 = new BasicDBObject();
+			document2.append("data proposta", "ventinovequattronovantuno");
+			document2.append("dita", 45);
+			collection.insert(document2);
+			
+			
+			
+			
+			
 			BasicDBObject searchQuery = new BasicDBObject();
-			searchQuery.put("isbn", 1935182870);
-			DBCursor cursor = collection.find(searchQuery);
+			searchQuery.put("data proposta", suT.dataEvento(date).toString());
+			
+			DBCursor cursor = collection.find();
 			while (cursor.hasNext()) {
 				System.out.println(cursor.next());
 			}
+			
 			System.out.println("Done");
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (MongoException e) {
+			e.printStackTrace();
+		}
+		catch (Exception e){
 			e.printStackTrace();
 		}
 	}
