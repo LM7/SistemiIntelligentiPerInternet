f957f776c34d9737d854096e2cba1bb4980bd2ef
Luca Tomaselli <luc.tomaselli@stud.uniroma3.it>
2015-02-07 20:28:39 +0100

    Aggiunto metodo per stampare il contenuto del db in un file

--- Sistemi/src/main/StampaQuery.java
+++ Sistemi/src/main/StampaQuery.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.io.PrintWriter;
+
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
@@ -7,7 +9,7 @@ import com.mongodb.MongoClient;
 
 public class StampaQuery {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		MongoClient mongo = null;
 		try {
 			mongo = new MongoClient("localhost", 27017);
@@ -17,11 +19,26 @@ public class StampaQuery {
 		DB db = mongo.getDB("db");
 		DBCollection collection = db.getCollection("collezione");
 		
+		PrintWriter out = new PrintWriter("contenutoDB.txt", "UTF-8");
+		
 		DBCursor cursor = collection.find();
+		int j = 1;
 		while (cursor.hasNext()) {
-			System.out.println(cursor.next());
+			String s = cursor.next().toString();
+			System.out.println(j+") "+s);
+			s = j+") "+s.substring(0,1) + s.substring(51);
+			s = s.replaceAll("\"data\"", "DATA");
+			s = s.replaceAll("\"data proposta\"", "DATA PROPOSTA");
+			s = s.replaceAll("\"evento_cantante\"", "\tPERSONA");
+			s = s.replaceAll("\"persona proposta\"", "PERSONA PROPOSTA");
+			s = s.replaceAll("\"luogo\"", "\tLUOGO");
+			s = s.replaceAll("\"luogo proposto\"", "LUOGO PROPOSTO");
+			s = s.replaceAll("\"url\"", "\t\tURL");
+			out.println(s);
+			j++;
 		}
 
+		out.close();
 		System.out.println("Done");
 	}
 }
