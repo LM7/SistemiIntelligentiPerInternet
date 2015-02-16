ec9afff38b5259fb90acdda9239dc931c568d159
Luca Tomaselli <luc.tomaselli@stud.uniroma3.it>
2015-02-16 15:03:48 +0100

    Nuova istanza del DB

---
+++ Sistemi/src/main/Principal2.java
@@ -0,0 +1,155 @@
+package main;
+
+import java.io.PrintWriter;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+import nertagme.NerTagme;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+
+import events.MsnSearchEngine;
+import suTime.SUTime;
+import boilerpipe.Boilerpipe;
+import lastFM.*;
+
+public class Principal2 {
+
+	public final static int numero_query = 1;
+	public final static String[] CITTA = {"Roma","Londra","New York","Los Angeles","Stoccolma","Parigi","Helsinki","Canberra","Chicago","Austin"};
+
+	public static void main(String[] args) {
+		int i;
+		int j = 0;
+
+		//Database
+		MongoClient mongo = null;
+		try {
+			mongo = new MongoClient("localhost", 27017);
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		DB db = mongo.getDB("db2");
+		DBCollection collection = db.getCollection("collezione2");	
+		
+		// svuota database
+		BasicDBObject x = new BasicDBObject();
+		collection.remove(x);
+
+
+		for(i=0;i<CITTA.length;i++) {
+
+			// 'totale' e' una lista di eventi [artista, luogo, data]
+			ArrayList<String[]> totale = geoMethods.eventsPusher(CITTA[i]);
+
+			for(String[] trio: totale) {
+				String evento_cantante = trio[0];
+				String luogo = trio[1];
+				String data = trio[2];
+
+
+				MsnSearchEngine se = new MsnSearchEngine();
+				String[] urls = se.getUrls(data+" "+evento_cantante, numero_query);
+				for(String s: urls) {
+					//Boilerpipe
+					Boilerpipe b = new Boilerpipe();
+					URL url = null;
+					try {
+						j++;
+						System.out.println("Stiamo a "+j);
+						url = new URL(s);	
+						String[] site = b.getText(url);
+						String title = site[0];
+						String text = site[1];				
+
+						HashSet<String> luoghi = null; 
+						
+						//SUTime
+						SUTime suT = new SUTime();
+						HashMap<Date, Integer> date = suT.getTime(title,text);
+						boolean continua = contieneDate(date.keySet(),data);
+
+						
+						//NERTAGME
+						String cosaMale = "";
+						if(continua) {
+							//lista di persone presa da NERTAGME hashMap.keySet
+							Set<String> persone = new HashSet<String>();					
+							continua = persone.contains(evento_cantante);
+							
+							if(continua){
+								//lista di luoghi presa da NERTAGME hashMap.keySet
+								luoghi = new HashSet<String>();
+							}
+							else
+								cosaMale = "No persona";
+						}
+						else
+							cosaMale = "No data";
+
+						
+						BasicDBObject document = new BasicDBObject();
+						document.put("url", url.toString());
+						
+						if(luoghi != null) 
+							document.put("luoghi", luoghi);
+						else {
+							document.put("xk male", cosaMale);
+							document.put("buono", false);
+						}
+
+						collection.insert(document);
+						
+					} catch (Exception e) {
+						//System.out.println(e.getMessage());
+					}
+
+				}
+			}
+		}
+		int k = 1;
+		DBCursor cursor = collection.find();
+		while (cursor.hasNext()) {
+			System.out.println(k+") "+cursor.next());
+			k++;
+		}
+
+		System.out.println("Done"+j);
+	}
+
+
+
+	private static boolean contieneDate(Set<Date> keySet, String data) {	
+		Locale.setDefault(Locale.ENGLISH);
+		SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MMMMMMMMMM dd yyyy");
+		//stampaLista(keySet,data);
+		for(Date d: keySet) {
+			String dataString = DATE_FORMAT.format(d);
+			if(dataString.equals(data))
+				return true;
+		}
+		return false;
+	}
+
+
+	private static void stampaLista(Set<Date> keySet, String data) {
+		SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MMMMMMMMMM dd yyyy");
+		System.out.print(data+" -> { ");
+		for(Date d: keySet) {
+			String dataString = DATE_FORMAT.format(d);
+			System.out.println(dataString + " ; ");
+		}
+		System.out.print("}");
+	}
+
+}
