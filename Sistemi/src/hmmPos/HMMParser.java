d39625857aaf6d919296d2680978b864b65b495e
Luca Tomaselli <luc.tomaselli@stud.uniroma3.it>
2015-03-06 18:53:28 +0100

    Aggiunto HMM-POS

---
+++ Sistemi/src/hmmPos/HMMParser.java
@@ -0,0 +1,89 @@
+package hmmPos;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class HMMParser {
+    
+    public static void main(String[] args){
+        HMMParser p = new HMMParser("data/test.pos");
+        p.parseTrainer();        
+    }
+    
+    Scanner scanner;
+    
+    HashMap<String, Integer> tagCounts = new HashMap<String, Integer>();
+    HashMap<String, HashMap<String, Integer>> wordCounts = new HashMap<String, HashMap<String,Integer>>();
+    HashMap<String, HashMap<String, Integer>> tagBigramCounts = new HashMap<String, HashMap<String,Integer>>();
+    HashMap<String, HashMap<String, Integer>> tagForWordCounts = new HashMap<String, HashMap<String, Integer>>();
+    String mostFreqTag = "";
+    int mostFreqTagCount = 0;
+    int numTrainingBigrams = 0;
+    
+    
+    public HMMParser(String filename){
+        File file = new File(filename);
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void parseTrainer(){
+        String prevTag = scanner.next();
+        scanner.next();
+       
+        while(scanner.hasNext()){
+            String currentTag = scanner.next();
+            String currentWord = scanner.next();
+            
+            addOne(tagCounts, currentTag);
+            addOne(wordCounts, currentTag, currentWord);
+            addOne(tagBigramCounts, prevTag, currentTag);
+            addOne(tagForWordCounts, currentWord, currentTag);
+            
+            if(tagCounts.get(currentTag) >= mostFreqTagCount){
+                mostFreqTagCount = tagCounts.get(currentTag);
+                mostFreqTag = currentTag;
+            }
+            
+            numTrainingBigrams++;
+            
+            prevTag = currentTag;
+        }
+    }
+    
+    public ArrayList<String> wordSequence(){
+        ArrayList<String> list = new ArrayList<String>();
+        while (scanner.hasNext()){
+            scanner.next();
+            list.add(scanner.next());
+        }
+        return list;
+    }
+    
+    // performs map[key1]++;
+    private void addOne(HashMap<String, Integer> map, String key1){
+        if(map.containsKey(key1)){
+            map.put(key1, map.get(key1)+1);
+        } else{
+            map.put(key1, 1);
+        }       
+    }   
+    
+    // performs map[key1][key2]++;
+    private void addOne(HashMap<String, HashMap<String, Integer>> map, String key1, String key2){
+        if(map.containsKey(key1)){
+            addOne(map.get(key1),key2);
+        } else {
+            HashMap<String, Integer> subMap = new HashMap<String, Integer>();
+            subMap.put(key2, 1);
+            map.put(key1, subMap);
+        }
+    }    
+}
