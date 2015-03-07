0d1fd1af8c147832555f8e86a8aa62a8bbcfa7f5
LM7 <lorenzo.martucci@hotmail.it>
2015-03-07 18:10:15 +0100

    Aggiunto metodo per titoli da file nella classe Parser

--- Sistemi/src/hmm/Parser.java
+++ Sistemi/src/hmm/Parser.java
@@ -1,6 +1,9 @@
 package hmm;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
@@ -17,6 +20,22 @@ public class Parser {
 		}
 		out.close();
 		
+	}
+	
+	private void parserForFileTitle(File titoli) throws Exception {
+		BufferedReader reader = new BufferedReader(new FileReader("contenutoDB.txt")); //titoli appena ottenuti
+		String line = reader.readLine();
+		PrintWriter outForHMM = new PrintWriter("trainForHMM.txt", "UTF-8"); //file che servira' per HMM
+		while (line!=null) {
+			outForHMM.println("<s> <s>");
+			String[] splits = line.split(" ");
+			for (String s: splits) {
+				s = s.replaceAll("#", " ");
+				outForHMM.println(s);
+			}
+		    line = reader.readLine();
+		}
+		outForHMM.close();
 	}
 
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
