912201b551177f22bb02c860c8e217cda516166a
Luca Tomaselli <luc.tomaselli@stud.uniroma3.it>
2015-03-08 18:46:03 +0100

    Risolti problemi per la data

--- Sistemi/src/suTime/SUTime_Titoli.java
+++ Sistemi/src/suTime/SUTime_Titoli.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Properties;
 
 import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.AnnotationPipeline;
 import edu.stanford.nlp.pipeline.POSTaggerAnnotator;
@@ -33,10 +34,19 @@ public class SUTime_Titoli {
 		pipeline.annotate(annotation);
 		List<CoreMap> timexAnnsAll = annotation.get(TimeAnnotations.TimexAnnotations.class);
 		for (CoreMap cm : timexAnnsAll) {
+			List<CoreLabel> tokens = cm.get(CoreAnnotations.TokensAnnotation.class);
+			
+			String textPreData = text.substring(0, tokens.get(0).get(CoreAnnotations.CharacterOffsetBeginAnnotation.class));
+			String textData = text.substring(tokens.get(0).get(CoreAnnotations.CharacterOffsetBeginAnnotation.class), tokens.get(tokens.size() - 1).get(CoreAnnotations.CharacterOffsetEndAnnotation.class));
+			String textPostData = text.substring(tokens.get(tokens.size() - 1).get(CoreAnnotations.CharacterOffsetEndAnnotation.class));
+			
 			String[] dataComposta = cm.toString().split(" ");
-			for(i=0;i<dataComposta.length;i++) {
-				text = text.replaceAll(dataComposta[i], "DDD#"+dataComposta[i]);
+			for(i=0;i<dataComposta.length;i++) {					
+				if(!dataComposta[i].equals("") && !dataComposta[i].equals(" ")) {
+					textData = textData.replaceAll(dataComposta[i], "DDD#"+dataComposta[i]);
+				}
 			}
+			text = textPreData + textData + textPostData;
 		}
 		return text;
 	}
@@ -44,8 +54,19 @@ public class SUTime_Titoli {
 	
 	public static void main(String[] args) {
 		SUTime_Titoli sutt = new SUTime_Titoli();
+		/*
 		String s = sutt.getTextTag(" Kumkum Bhagya Episode 231 February 27 2015 Preview Zeetv » | India Video News - VideoSamachar.com ");
 		System.out.println(s);
+		String ss = sutt.getTextTag("Baxter, International Inc. (BAX) Ex-Dividend Date Scheduled for March 09, 2015 - NASDAQ.com");
+		System.out.println(ss);
+		*/
+		String sss = sutt.getTextTag("8 (Sunday,  10 April 2018) Connie, Stefny, Steven Letigre Brooklyn concert tickets, Output Club Brooklyn (Sunday,  8 March 2015) - 5gig.com");
+		System.out.println("8 (Sunday,  10 April 2018) Connie, Stefny, Steven Letigre Brooklyn concert tickets, Output Club Brooklyn (Sunday,  8 March 2015) - 5gig.com");
+		System.out.println(sss);
+		
+		String ssss = sutt.getTextTag("The Darkness at Whelan's|08 March 2015 - music listings for Dublin, Ireland - entertainment.ie");
+		System.out.println("The Darkness at Whelan's|08 March 2015 - music listings for Dublin, Ireland - entertainment.ie");
+		System.out.println(ssss);
 	}
 
 }
